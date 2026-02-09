47cb2b9f214bfa310c9d8d15927cf254d366a3fa
Farhana Malem <farhana.malem@gmail.com>
2026-02-10 04:01:44 +0700

    add student homepage

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let href = '#';
+</script>
+
+<a
+	{href}
+	class="border-primary hover:bg-primary/5 flex size-75 flex-col items-center justify-center rounded-4xl border-3"
+>
+
+		<slot name="icon" />
+
+
+	<div class="text-primary text-center text-xl font-medium px-7 break-all">
+		<slot />
+	</div>
+</a>
---
+++ src/routes/student/homepage/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Card from '$lib/components/card.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center pb-7">
+	<div class="text-primary text-6xl font-bold">00:00:00</div>
+	<div>หมดเขต dd MM yy</div>
+</div>
+
+<div class="pb-7 text-3xl font-bold">โปรดเลือกทำรายการ</div>
+
+<div class="flex flex-wrap gap-x-10 gap-y-7">
+	<Card href="">
+		<svg
+			slot="icon"
+			class="text-primary h-[100px] w-[100px]"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="1"
+				d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+		</svg>
+
+		เสนอตัวเองเป็นนิสิตดีเด่น
+	</Card>
+
+	<Card href="">
+		<svg
+			slot="icon"
+			class="text-primary h-[100px] w-[100px]"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="1"
+				d="M18 6H6m12 4H6m12 4H6m12 4H6"
+			/>
+		</svg>
+
+		รายการที่เคยเสนอ
+	</Card>
+</div>

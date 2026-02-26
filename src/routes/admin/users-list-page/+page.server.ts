9c425d1ada817a90b322d4a8535f8601afb52249
Ekkamon Munsri <ekkb.contact@gmail.com>
2026-02-27 05:39:37 +0700

    🐛 fix(CI/CD): change Dockerfile

--- src/routes/admin/users-list-page/+page.server.ts
+++ src/routes/admin/users-list-page/+page.server.ts
@@ -4,15 +4,26 @@ import { metadata } from '@iconify-json/mdi/index.js';
 
 export const load: PageServerLoad = async ({ url }) => {
 	try {
-        const page = url.searchParams.get('page') ?? '1';
-		const response = await apiClient.get(`/users?page=${page}`); 
+		const page = url.searchParams.get('page') ?? '1';
+		const response = await apiClient.get(`/users?page=${page}`);
 
 		if (response.status === 200) {
 			return {
-				users: response.data.data,
-                links: response.data.links,
-                meta: response.data.meta
+				users: response.data.data ?? [],
+				links: response.data.links ?? {},
+				meta: response.data.meta ?? {}
 			};
 		}
-	} catch (err: any) {}
+	} catch (err) {
+		console.error(err);
+	}
+
+	return {
+		users: [],
+		links: {},
+		meta: {
+			current_page: 1,
+			last_page: 1
+		}
+	};
 };
